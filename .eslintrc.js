f811dda753addca6c710366146ca0d343f18f3a9
Christopher Amin <camin@ripe.net>
2022-02-17 12:07:34 +0100

    prettier eslint (force of habit)

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+  },
+  extends: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  globals: {}
+}
